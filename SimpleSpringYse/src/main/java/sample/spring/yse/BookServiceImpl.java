474ab3eed6f25b6c08fd472b9839bffa48f54aec
Jeongc2022 <jeongc2022@gmail.com>
2022-01-07 20:43:55 +0900

    "책 목록 만들기 1"

--- SimpleSpringYse/src/main/java/sample/spring/yse/BookServiceImpl.java
+++ SimpleSpringYse/src/main/java/sample/spring/yse/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package sample.spring.yse;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +35,9 @@ public class BookServiceImpl implements BookService {
 	public boolean remove(Map<String, Object> map) {
 		int affectRowCount = this.bookDao.delete(map);
 		return affectRowCount == 1;
+	}
+	
+	public List<Map<String, Object>> list(Map<String, Object> map) {
+		return this.bookDao.selectList(map);
 	}
 }
